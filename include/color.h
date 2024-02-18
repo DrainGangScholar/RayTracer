7d9cd28895a4147ec44a788056e69ffc306b803c
DrainGangScholar <veljko.markovic@elfak.rs>
2024-02-18 18:25:33 +0100

    dan drugi :D

--- include/color.h
+++ include/color.h
@@ -6,8 +6,8 @@
 
 using color = vec3;
 
-void write_color(std::ostream &out, color pixel_color) {
-	out<<static_cast<int>(255.999*pixel_color.x())<< ' '
-	   <<static_cast<int>(255.999*pixel_color.y())<< ' '
-	   <<static_cast<int>(255.999*pixel_color.z())<< ' '<<'\n';
+inline void write_color(std::ostream &out, color pixel_color) {
+  out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+      << static_cast<int>(255.999 * pixel_color.y()) << ' '
+      << static_cast<int>(255.999 * pixel_color.z()) << ' ' << '\n';
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,76 @@
-#include"../include/vec3.h"
-#include"../include/color.h"
-#include"../include/ray.h"
+#include "../include/color.h"
+#include "../include/ray.h"
+#include "../include/vec3.h"
 
-color ray_color(const ray& r) {
-	vec3 unit_direction = unit_vector(r.direction());
-	auto a = 0.5*(unit_direction.y()+1.0);
-	return (1.0-a)*color(1.0,1.0,1.0)+a*color(0.5,0.7,1.0);
+double hit_sphere(const point3 &center, double radius, const ray &r) {
+  vec3 oc = r.origin() - center; // A-C
+  auto a = r.direction().length_squared();
+  auto half_b = dot(oc, r.direction());
+  auto c = oc.length_squared() - (radius * radius);
+  auto discriminant = half_b * half_b - a * c;
+  if (discriminant < 0.0) {
+    return -1.0;
+  } else {
+    return (-half_b - sqrt(discriminant)) / a;
+  }
+}
+
+color ray_color(const ray &r) {
+  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+  if (t > 0.0) {
+    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+  }
+
+  vec3 unit_direction = unit_vector(r.direction());
+  auto a = 0.5 * (unit_direction.y() + 1.0);
+  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
 }
 
 int main() {
-	//Image
-	auto aspect_ratio = 16.0/9.0;
+  // Image
+  auto aspect_ratio = 16.0 / 9.0;
 
-	int image_width=400;
-	int image_height = static_cast<int>(image_width/aspect_ratio);
-	image_height = (image_height<1)?1:image_height;
+  int image_width = 400;
+  int image_height = static_cast<int>(image_width / aspect_ratio);
+  image_height = (image_height < 1) ? 1 : image_height;
 
-	//Camera
-	auto focal_length = 1.0;
-	auto viewport_height = 2.0;
-	auto viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
-	auto camera_center = point3(0,0,0);
+  // Camera
+  auto focal_length = 1.0;
+  auto viewport_height = 2.0;
+  auto viewport_width =
+      viewport_height * (static_cast<double>(image_width) / image_height);
+  auto camera_center = point3(0, 0, 0);
 
-	//Horizontal and vertical edges of viewport 
-	auto viewport_u = vec3(viewport_width,0,0);
-	auto viewport_v = vec3(0,-viewport_height,0);
+  // Horizontal and vertical edges of viewport
+  auto viewport_u = vec3(viewport_width, 0, 0);
+  auto viewport_v = vec3(0, -viewport_height, 0);
 
+  // Delta vectors
+  auto pixel_delta_u = viewport_u / image_width;
+  auto pixel_delta_v = viewport_v / image_height;
 
-	//Delta vectors
-	auto pixel_delta_u = viewport_u / image_width;
-	auto pixel_delta_v = viewport_v / image_height;
+  // Upper left pixel location
+  auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) -
+                             viewport_u / 2 - viewport_v / 2;
 
-	//Upper left pixel location
-	auto viewport_upper_left = camera_center
-		- vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+  auto pixel100_loc =
+      viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
-	auto pixel100_loc = viewport_upper_left+0.5*(pixel_delta_u+pixel_delta_v);
+  // Render
+  std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+  for (int i = 0; i < image_height; i++) {
+    std::clog << "\rScanlines remaining: " << (image_height - i) << ' '
+              << std::flush;
+    for (int j = 0; j < image_width; j++) {
+      auto pixel_center =
+          pixel100_loc + (i * pixel_delta_v) + (j * pixel_delta_u);
+      auto ray_direction = pixel_center - camera_center;
+      ray r(camera_center, ray_direction);
 
-	//Render
-	std::cout<< "P3\n" << image_width << ' ' <<image_height << "\n255\n";
-	for(int i=0;i<image_height;i++) {
-		std::clog<<"\rScanlines remaining: " << (image_height-i) << ' ' <<std::flush;
-		for(int j=0;j<image_width;j++) {
-			auto pixel_center = pixel100_loc + (i*pixel_delta_v) + (j*pixel_delta_u);
-			auto ray_direction = pixel_center - camera_center;
-			ray r(camera_center, ray_direction);
-
-			color pixel_color = ray_color(r);
-			write_color(std::cout,pixel_color);
-		}
-	}
-	std::clog << "\rDone.                 \n";
+      color pixel_color = ray_color(r);
+      write_color(std::cout, pixel_color);
+    }
+  }
+  std::clog << "\rDone.                 \n";
 }
