1d762820c7b99e1d270387447ccc749ecf9d235b
DrainGangScholar <veljko.markovic@elfak.rs>
2024-02-19 21:45:03 +0100

    :DD

---
+++ include/sphere.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "hittable.h"
+#include "vec3.h"
+
+class sphere : public hittable {
+public:
+  sphere(point3 _center, double _radius) : center(_center), radius(_radius) {}
+
+  bool hit(const ray &r, double ray_tmin, double ray_tmax,
+           hit_record &rec) const override {
+    vec3 oc = r.origin() - center;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0) {
+      return false;
+    }
+
+    auto sqrtd = sqrt(discriminant);
+
+    auto root = (-half_b - sqrtd) / a;
+    if (root <= ray_tmin || root >= ray_tmax) {
+      root = (-half_b + sqrtd) / a;
+      if (root <= ray_tmin || root >= ray_tmax) {
+        return false;
+      }
+    }
+
+    rec.t = root;
+    rec.p = r.at(rec.t);
+    vec3 outward_normal = (rec.p - center) / radius;
+    rec.set_face_normal(r,outward_normal);
+
+    return true;
+  }
+
+private:
+  point3 center;
+  double radius;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,19 @@
+#include"../include/utility.h"
+
 #include "../include/color.h"
-#include "../include/ray.h"
-#include "../include/vec3.h"
+#include "../include/hittable.h"
+#include "../include/hittable_list.h"
+#include "../include/sphere.h"
 
-double hit_sphere(const point3 &center, double radius, const ray &r) {
-  vec3 oc = r.origin() - center; // A-C
-  auto a = r.direction().length_squared();
-  auto half_b = dot(oc, r.direction());
-  auto c = oc.length_squared() - (radius * radius);
-  auto discriminant = half_b * half_b - a * c;
-  if (discriminant < 0.0) {
-    return -1.0;
-  } else {
-    return (-half_b - sqrt(discriminant)) / a;
-  }
-}
+color ray_color(const ray &r, const hittable& world) {
+	hit_record rec;
+	if(world.hit(r,0,infinity,rec)){
+		return 0.5*(rec.normal+color(1,1,1));
+	}
 
-color ray_color(const ray &r) {
-  auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-  if (t > 0.0) {
-    vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
-    return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
-  }
-
-  vec3 unit_direction = unit_vector(r.direction());
-  auto a = 0.5 * (unit_direction.y() + 1.0);
-  return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+	vec3 unit_direction = unit_vector(r.direction());
+	auto a = 0.5*(unit_direction.y()+1.0);
+	return (1.0-a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
 }
 
 int main() {
@@ -34,6 +23,12 @@ int main() {
   int image_width = 400;
   int image_height = static_cast<int>(image_width / aspect_ratio);
   image_height = (image_height < 1) ? 1 : image_height;
+
+  //World
+  hittable_list world;
+
+  world.add(make_shared<sphere>(point3(0,0,-1),0.5));
+  world.add(make_shared<sphere>(point3(0,-100.5,-1),100));
 
   // Camera
   auto focal_length = 1.0;
@@ -68,7 +63,7 @@ int main() {
       auto ray_direction = pixel_center - camera_center;
       ray r(camera_center, ray_direction);
 
-      color pixel_color = ray_color(r);
+      color pixel_color = ray_color(r,world);
       write_color(std::cout, pixel_color);
     }
   }
