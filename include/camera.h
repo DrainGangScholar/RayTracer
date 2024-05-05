58e6203606db06a3783197074ab0551f4f007797
DrainGangScholar <veljko.markovic@elfak.rs>
2024-05-05 13:41:08 +0200

    added omp :D

--- include/camera.h
+++ include/camera.h
@@ -139,7 +139,7 @@ private:
       }
       return color(0, 0, 0);
     }
-
+    // TODO:render background instead of this
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
